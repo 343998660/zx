02ce19a3c09e147c98c55e2047c69c99c28e0009
dongyifan <343998660@qq.com>
2020-07-16 14:16:09 +0800

    第一次创建项目文件

---
+++ src/test/java/com/cinsos/zx/ZxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cinsos.zx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
